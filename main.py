98b626c7ca7bb91cae5ac204326f97f4b7918760
Antonio Urdaneta <123696971+aaurdanet@users.noreply.github.com>
2024-04-09 21:08:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ window.title("Password Manager")
 window.config(padx=50, pady=50)
 
 canvas = Canvas(width=200, height=200)
-logo_img = PhotoImage(file="output-onlinepngtools(1).png")
+logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(column=1, row=0)
 
